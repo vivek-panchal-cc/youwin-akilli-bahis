9bf0840f668cf0692758e3bb819d4f5e357910fd
CCT Macmini <>
2023-08-02 18:08:32 +0530

    Link added on Header to redirect on homepage

--- app/src/components/common/header/AppHeader.jsx
+++ app/src/components/common/header/AppHeader.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import logoImg from '../../../assets/images/logo.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import logoImg from "../../../assets/images/logo.svg";
 
 /**
- * 
+ *
  * AppHeader - common component use for show header in layout.
- * 
+ *
  */
-
 
 const AppHeader = () => {
   return (
-    <div className='header'>
-      <div className='logo_img'>
-        <img src={logoImg} alt="main logo"/>
-      </div>
-      <h4>AKILLI BAHIS</h4>
+    <div className="header">
+      <Link to="/">
+        <div className="logo_img">
+          <img src={logoImg} alt="main logo" />
+        </div>
+      </Link>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <h4>AKILLI BAHIS</h4>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default AppHeader
+export default AppHeader;
